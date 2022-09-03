6d53b4e7f7db0e65d0fa74f990e584a1da6dad76
alejandro <agranadosb55199@outlook.com>
2022-09-03 10:32:06 +0200

    feat(Puzzle8MnistGenerator): Implement constructor and get function

--- tfm/data/puzzle.py
+++ tfm/data/puzzle.py
@@ -1,7 +1,10 @@
+import random
 from typing import List, Tuple
 
 import numpy as np
 import torch
+import torchvision
+from torchvision import transforms
 
 
 class Puzzle8MnistGenerator:
@@ -9,11 +12,20 @@ class Puzzle8MnistGenerator:
 
     Parameters
     ----------
-    size: int
-        Size of the image representing the puzzle."""
+    train: bool = True
+        Indicates if the split of the dataset if for training or not."""
 
-    def __init__(self, size: int):
-        self.size = size
+    def __init__(self, train: bool = True):
+        self.train = train
+        self.dataset = torchvision.datasets.MNIST(
+            root="./data", train=train, download=True, transform=transforms.ToTensor()
+        )
+        self.indices = {index: [] for index in range(10)}
+        for index in range(len(self.dataset)):
+            _, digit = self.dataset[index]
+            self.indices[digit].append(index)
+        del self.indices[9]
+        self.base_image = torch.zeros((28 * 3, 28 * 3))
 
     def get(self, ordered: bool = False) -> Tuple[torch.Tensor, List[int]]:
         """Returns a random generated 8-puzzle.
@@ -28,3 +40,20 @@ class Puzzle8MnistGenerator:
         Tuple[np.ndarray, List[int]]
             8-Puzzle generated.
             Order of the digits on the mnist puzzle."""
+        digits_selection = [random.choice(self.indices[digit]) for digit in range(9)]
+
+        if not ordered:
+            random.shuffle(digits_selection)
+
+        digits = []
+        for column in range(3):
+            for row in range(3):
+                ymin = column * 28
+                xmin = row * 28
+                ymax = ymin + 28
+                xmax = xmin + 28
+                image, digit = self.dataset[digits_selection[row * 3 + column]]
+                self.base_image[xmin:xmax, ymin:ymax] = image
+                digits.append(digit)
+
+        return self.base_image, digits
