c2986ea16ca021dd0eab0af85a1a4ef6bdad83af
alejandro <agranadosb55199@outlook.com>
2022-09-03 10:31:42 +0200

    fix(puzzle): Fix typo error puzzle tests

--- tests/data/test_puzzle.py
+++ tests/data/test_puzzle.py
@@ -7,7 +7,7 @@ from tfm.data.puzzle import Puzzle8MnistGenerator
 
 class TestGet(TestCase):
     def setUp(self) -> None:
-        self.generator = Puzzle8MnistGenerator(28 * 3)
+        self.generator = Puzzle8MnistGenerator()
 
     def test_get_correct_types(self):
         ordered = False
@@ -18,16 +18,16 @@ class TestGet(TestCase):
         self.assertTrue(isinstance(order, list))
         self.assertTrue(isinstance(order[0], int))
 
-    def test_get_unordered_puzzle(self):
-        ordered = False
-
-        puzzle, order = self.generator.get(ordered=ordered)
-
-        self.assertListEqual(list(sorted(order)), list(range(9)))
-
     def test_get_ordered_puzzle(self):
         ordered = True
 
         puzzle, order = self.generator.get(ordered=ordered)
 
-        self.assertListEqual(order, list(range(9)))
+        self.assertListEqual(list(sorted(order)), list(range(9)))
+
+    def test_get_unordered_puzzle(self):
+        ordered = False
+
+        puzzle, order = self.generator.get(ordered=ordered)
+
+        self.assertEqual(sum(order), sum(list(range(9))))
