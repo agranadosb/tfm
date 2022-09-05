6a1b519841301de5c9a6aac36edc8005a3a7a9d7
alejandro <agranadosb55199@outlook.com>
2022-09-05 22:36:14 +0200

    feat(main): Add main function to test the development of the project

--- __main__.py
+++ __main__.py
@@ -1,11 +1,17 @@
+import time
+
 from torchvision import transforms
 
 from tfm.data.puzzle import Puzzle8MnistGenerator
 from matplotlib import pyplot as plt
 
+from tfm.plots.images import plot_images
 
 if __name__ == "__main__":
+    init = time.time()
     p = Puzzle8MnistGenerator()
-    for i in range(10):
-        plt.imshow(transforms.ToPILImage()(p.get(ordered=True)[0]))
-        plt.show()
+    print(f"Create object: {time.time() - init}")
+    init = time.time()
+    images = [transforms.ToPILImage()(p.get()[0]) for _ in range(10)]
+    print(f"Load images: {time.time() - init}")
+    plot_images(images)
