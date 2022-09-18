21853e1e7b614701e1e72a43c2654c6066bc00ed
alejandro <agranadosb55199@outlook.com>
2022-09-18 15:50:49 +0200

    feat(utils.puzzle): Add tests for `has_correct_order` function

---
+++ tests/utils/test_puzzle.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from tfm.utils.puzzle import has_correct_order
+
+
+class TestUtilsPuzzle(TestCase):
+    def test_has_correct_order_correct(self):
+        order = [0, 1, 2, 6, 7, 8, 3, 4, 5]
+
+        result = has_correct_order(order)
+
+        self.assertTrue(result)
+
+    def test_has_correct_order_correct_order_incorrect_length(self):
+        order = [0, 1, 2]
+
+        result = has_correct_order(order)
+
+        self.assertFalse(result)
+
+    def test_has_correct_order_incorrect_order_incorrect_length(self):
+        order = [0, 1, 0]
+
+        result = has_correct_order(order)
+
+        self.assertFalse(result)
+
+    def test_has_correct_order_incorrect_order_correct_length(self):
+        order = [0, 1, 2, 6, 7, 8, 3, 4, 0]
+
+        result = has_correct_order(order)
+
+        self.assertFalse(result)
