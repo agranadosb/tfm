5d7a624fcecd780a0b6992d80b8b08d168d44bf9
alejandro <agranadosb55199@outlook.com>
2023-07-15 17:10:15 +0200

    feat(constants): Add a literals per dataset.

--- tfm/constants/__init__.py
+++ tfm/constants/__init__.py
@@ -6,22 +6,25 @@ LEFT_RIGHT_MOVEMENTS = [1, -1]
 TOP_BOT_MOVEMENTS = [3, -3]
 MOVEMENTS = [*LEFT_RIGHT_MOVEMENTS, *TOP_BOT_MOVEMENTS]
 
+PUZZLE_DATASET = "puzzle8"
+LIGHTS_DATASET = "lights-out"
+
 LABEL_TO_STRING = {
-    "puzzle8": {
+    PUZZLE_DATASET: {
         0: "Right",
         1: "Left",
         2: "Bottom",
         3: "Top",
     },
-    "lights-out": {i: f"Light {i}" for i in range(25)},
+    LIGHTS_DATASET: {i: f"Light {i}" for i in range(25)},
 }
 MOVEMENT_TO_LABEL = {
-    "puzzle8": {1: 0, -1: 1, 3: 2, -3: 3},
-    "lights-out": {i: i for i in range(25)},
+    PUZZLE_DATASET: {1: 0, -1: 1, 3: 2, -3: 3},
+    LIGHTS_DATASET: {i: i for i in range(25)},
 }
 LABEL_TO_MOVEMENT = {
-    "puzzle8": {value: key for key, value in MOVEMENT_TO_LABEL["puzzle8"].items()},
-    "lights-out": {value: key for key, value in MOVEMENT_TO_LABEL["lights-out"].items()},
+    PUZZLE_DATASET: {value: key for key, value in MOVEMENT_TO_LABEL[PUZZLE_DATASET].items()},
+    LIGHTS_DATASET: {value: key for key, value in MOVEMENT_TO_LABEL[LIGHTS_DATASET].items()},
 }
 
 BLOCKS = {"conv": ConvBlock, "residual": ResidualBlock}
