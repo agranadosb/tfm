7932ca9ddbc374df440565604c1f2b097ef7d229
alejandro <agranadosb55199@outlook.com>
2023-07-15 17:10:54 +0200

    feat(main): Add function to get automatically all functions inside the cli modules.

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 import os
+import sys
+
 import fire
 import torch
+from fire.core import FireExit
 
-from tfm.cli.inference import random_prediction, move, show_possibilities
-from tfm.cli.training import train, save_checkpoint
-from tfm.cli.tuning import hyperparameter_tuning
+from tfm.utils.cli import get_cli
 
 os.environ["WANDB_API_KEY"] = "local-c0e251ebaef9040fd778217f91fcff426f891404"
 
@@ -12,11 +13,7 @@ torch.set_float32_matmul_precision('medium')
 
 
 if __name__ == "__main__":
-    fire.Fire({
-        "train": train,
-        "hyperparameter-tuning": hyperparameter_tuning,
-        "save-checkpoint": save_checkpoint,
-        "random-prediction": random_prediction,
-        "move": move,
-        "show-possibilities": show_possibilities,
-    })
+    try:
+        fire.Fire(get_cli())
+    except FireExit:
+        sys.exit(1)
---
+++ tfm/utils/cli.py
@@ -0,0 +1,24 @@
+import inspect
+from typing import Callable, Dict
+
+from tfm import cli
+
+
+def get_cli() -> Dict[str, Callable]:
+    # DO NOT REMOVE THIS IMPORTS, THEY ARE USED BY
+    # `inspect.getmembers(cli, inspect.ismodule)`
+    from tfm.cli import inference, training, tuning
+
+    modules = inspect.getmembers(cli, inspect.ismodule)
+    modules_ids = [id(i[1]) for i in modules]
+    fire_mapping = {}
+
+    for module in modules:
+        fire_mapping.update({
+            function[0].replace("_", "-"): function[1]
+            for function in inspect.getmembers(module[1], inspect.isfunction)
+            if id(inspect.getmodule(function[1])) in modules_ids}
+        )
+
+    return fire_mapping
+
